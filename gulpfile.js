5b25cc2119aa87c5701d45132c93b783639aa91a
jeremy_jeanne <jeremy.jeanne@4dconcept.fr>
2025-10-07 00:13:49 +0200

    Fix gulpfile for Gulp 4 and ES6 - Build successful

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,53 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify'),
 	browserify = require('browserify'),
 	watchify = require('watchify'),
 	buffer = require('vinyl-buffer'),
 	connect = require('gulp-connect'),
-	source = require('vinyl-source-stream'),
-	jshint = require('gulp-jshint');
+	source = require('vinyl-source-stream');
 
-gulp.task('default', ['connect', 'compile']);
-
-paths = {
+const paths = {
 	entry: './client/src/main.js',
 	dist: './client/dist/'
 };
 
-gulp.task('compile', function() {
+// Gulp 4: Use function for compile task
+function compile(done) {
 	var bundler = browserify(paths.entry, watchify.args);
 
 	var bundle = function() {
+		console.log('Bundling...');
 		return bundler
 			.bundle()
+			.on('error', function(err) {
+				console.error(err.toString());
+				this.emit('end');
+			})
 			.pipe(source('bomb_arena.min.js'))
 			.pipe(buffer())
-			.pipe(uglify())
-			.pipe(connect.reload())
+			// Skip uglify - doesn't support ES6
 			.pipe(gulp.dest(paths.dist))
-	}
+			.pipe(connect.reload());
+	};
 
 	bundler = watchify(bundler);
-	bundler.on('update', bundle);
+	bundler.on('update', function() {
+		console.log('File changed, rebundling...');
+		bundle();
+	});
+	bundler.on('log', console.log);
 
 	return bundle();
-});
+}
 
-gulp.task('connect', function() {
-	connect.server({
+function connectServer() {
+	return connect.server({
 		root: [__dirname + '/client'],
 		port: 9000,
 		livereload: true
 	});
-});
+}
+
+// Gulp 4: Export tasks
+exports.compile = compile;
+exports.connect = connectServer;
+exports.default = gulp.parallel(connectServer, compile);
